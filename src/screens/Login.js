00a319b77b4dc6f817c176fcbace57862c69fb14
JINHUI KIM <xzdfor@naver.com>
2021-10-13 01:16:39 +0900

    sign up and login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,32 +1,123 @@
+import { gql, useMutation } from "@apollo/client";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useForm } from "react-hook-form";
+import { useLocation } from "react-router-dom";
 import styled from "styled-components";
-import { darkModeVar, isLoggedInVar } from "../apollo";
+import { logUserIn } from "../apollo";
+import AuthLayout from "../components/auth/AuthLayout";
+import BottomBox from "../components/auth/BottomBox";
+import Button from "../components/auth/Button";
+import FormBox from "../components/auth/FormBox";
+import FormError from "../components/auth/FormError";
+import Input from "../components/auth/Input";
+import PageTitle from "../components/PageTitle";
+import routes from "../routes";
 
-const Title = styled.h1`
-  color: ${(props) => props.theme.fontColor};
+const Notification = styled.div`
+  color: #2ecc71;
 `;
 
-const Container = styled.div`
-  margin-top: 20px;
-  text-align: center;
-`;
-
-const ToggleButton = styled.button`
-  margin: 5px;
-  padding: 10px;
+const LOGIN_MUTATION = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      ok
+      token
+      error
+    }
+  }
 `;
 
 function Login() {
-  const toggleColorMode = () => darkModeVar(!darkModeVar());
+  const location = useLocation();
+  const {
+    register,
+    handleSubmit,
+    formState,
+    getValues,
+    setError,
+    clearErrors,
+  } = useForm({
+    mode: "onChange",
+    defaultValues: {
+      username: location?.state?.username || "",
+      password: location?.state?.password || "",
+    },
+  });
+  const onCompleted = (data) => {
+    const {
+      login: { ok, error, token },
+    } = data;
+    if (!ok) {
+      return setError("result", {
+        message: error,
+      });
+    }
+    if (token) {
+      logUserIn(token);
+    }
+  };
+  const [login, { loading }] = useMutation(LOGIN_MUTATION, {
+    onCompleted,
+  });
+  const onSubmitValid = (data) => {
+    if (loading) {
+      return;
+    }
+    const { username, password } = getValues();
+    login({
+      variables: { username, password },
+    });
+  };
   return (
-    <Container>
-      <Title>Login</Title>
-      <ToggleButton onClick={() => isLoggedInVar(true)}>
-        Log in now!
-      </ToggleButton>
-      <ToggleButton onClick={toggleColorMode}>
-        {darkModeVar() ? "To light" : "To dark"}
-      </ToggleButton>
-    </Container>
+    <AuthLayout>
+      <PageTitle title="Login" />
+      <FormBox>
+        <div>
+          <FontAwesomeIcon icon={faCoffee} size="3x" />
+        </div>
+        <Notification>{location?.state?.message}</Notification>
+        <form onSubmit={handleSubmit(onSubmitValid)}>
+          <Input
+            {...register("username", {
+              required: "Username is required",
+              minLength: {
+                value: 5,
+                message: "Username should be longer than 5 chars.",
+              },
+            })}
+            onKeyDown={() => clearErrors("result")}
+            name="username"
+            type="text"
+            placeholder="Username"
+            hasError={Boolean(formState.errors?.username?.message)}
+          />
+          <FormError message={formState.errors?.username?.message} />
+          <Input
+            {...register("password", {
+              required: "Password is required.",
+            })}
+            onFocus={() => clearErrors("result")}
+            name="password"
+            type="password"
+            placeholder="Password"
+            hasError={Boolean(formState.errors?.password?.message)}
+          />
+          <FormError message={formState.errors?.password?.message} />
+          <Button
+            type="submit"
+            value={loading ? "Loading..." : "Log in"}
+            disabled={!formState.isValid || loading}
+          />
+          <FormError message={formState.errors?.result?.message} />
+        </form>
+      </FormBox>
+      <BottomBox
+        cta="Don't have an account?"
+        linkText="Sign up"
+        link={routes.signUp}
+      />
+    </AuthLayout>
   );
 }
 export default Login;
